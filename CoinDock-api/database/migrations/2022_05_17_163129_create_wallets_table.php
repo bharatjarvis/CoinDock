45363f249b165aff5b49757adcabf82ff92bc686
mouli <mouliballanki143@gmail.com>
2022-05-17 22:19:49 +0530

    Wallets and coins schemas added

---
+++ CoinDock-api/database/migrations/2022_05_17_163129_create_wallets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('coin_id');
+            $table->foreign('coin_id')->references('id')->on('coins');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('wallet_id');
+            $table->integer('balance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
