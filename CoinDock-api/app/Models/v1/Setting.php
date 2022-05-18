925e56cbc6cf3a1caa8fb1a391979fdbc068e5d2
gayathrim997 <95843431+gayathrim997@users.noreply.github.com>
2022-05-18 01:53:50 -0700

    recovry keys and settings table migrations done

---
+++ CoinDock-api/app/Models/v1/Setting.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\v1;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    use HasFactory;
+}
