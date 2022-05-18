ba3649a2ecbfa8cfd6544b9aec267bdf782b2d4f
MouliBallanki <mouliballanki@143gmail.com>
2022-05-18 15:55:07 +0530

    changes done in coins and user migrations

--- CoinDock-api/database/migrations/2014_10_12_000000_create_users_table.php
+++ CoinDock-api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->integerIncrements('id');
             $table->string('first_name');
             $table->string('last_name');
             $table->enum('user_type',['customer','admin']);
--- CoinDock-api/database/migrations/2022_05_17_161749_create_coins_table.php
+++ CoinDock-api/database/migrations/2022_05_17_161749_create_coins_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('coins', function (Blueprint $table) {
-            $table->id();
+            $table->integerIncrements('id');
             $table->string('name');
             $table->timestamps();
         });
