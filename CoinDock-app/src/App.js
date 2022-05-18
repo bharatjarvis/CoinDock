3a361b51ebc20ec01ebb539657094087f3ed5a88
PavaniJavapu <97446162+PavaniJavapu@users.noreply.github.com>
2022-05-18 03:35:52 -0700

    initial

--- CoinDock-app/src/App.js
+++ CoinDock-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Sign up page</h1>
+     
       </header>
     </div>
   );
