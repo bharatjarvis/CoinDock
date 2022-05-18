a95757f4323a7378ab6eb3bede326818a15242fe
MouliBallanki <mouliballanki@143gmail.com>
2022-05-18 21:26:34 +0530

    users table added in v1 folder and fiilables added in wallet and coin models

--- CoinDock-api/app/Models/v1/Wallet.php
+++ CoinDock-api/app/Models/v1/Wallet.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Wallet extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'coin_id',
+        'user_id',
+        'wallet_id',
+        'balance'
+    ];
 }
