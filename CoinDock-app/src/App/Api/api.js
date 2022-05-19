4827f8532c176e41570d1cc3d83b25fb102fc945
padala_sita <sita.padala@loqbox.co.uk>
2022-05-19 21:48:30 +0530

    Added API domain to .env.development

--- CoinDock-app/src/App/Api/api.js
+++ CoinDock-app/src/App/Api/api.js
@@ -1,7 +1,12 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const baseApi = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: '/'}),
+
+    baseQuery: fetchBaseQuery({baseUrl: process.env.REACT_APP_API_DOMAIN}), 
+    endpoints: (build)=> {
+        return{}
+    }
 })
+
 
 export default baseApi
