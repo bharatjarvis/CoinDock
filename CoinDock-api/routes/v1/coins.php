4d8d5cbb4acac2b8c882188ea59fb2df273f43c1
MouliBallanki <mouliballanki@143gmail.com>
2022-06-16 15:42:28 +0530

    comments added for functions

--- CoinDock-api/routes/v1/coins.php
+++ CoinDock-api/routes/v1/coins.php
@@ -16,10 +16,54 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'coins', 'middleware' => 'auth:api'], function () {
 
+
+    /*
+    For        : Create a Coin
+    RouteName  : /coins/
+    Method     : POST
+    Access     : Private
+    */
     Route::post('/',[CoinController::class , 'store']);
+
+
+    
+    /*
+    For        : Listing all the coins
+    RouteName  : /coins/
+    Method     : GET
+    Access     : Private
+    */
     Route::get('/', [CoinController::class , 'index']);
+
+
+    
+    /*
+    For        : Show a particular Coin
+    RouteName  : /coins/{user}
+    Method     : SHOW
+    Access     : Private
+    */
     Route::get('/{user}',[CoinController::class , 'show']);
+
+
+    
+    /*
+    For        : Updating Coin Details
+    RouteName  : /coins/{user}
+    Method     : PUT
+    Access     : Private
+    */
     Route::put('/{user}',[CoinController::class , 'update']);
+
+
+    
+    /*
+    For        : Delete a Coin
+    RouteName  : /coins/{user}
+    Method     : DELETE
+    Access     : Private
+    */
     Route::delete('/{user}',[CoinController::class , 'delete']);
 
+    
 });
