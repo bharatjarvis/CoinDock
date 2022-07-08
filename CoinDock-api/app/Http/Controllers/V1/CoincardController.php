b7691e6230d5b7832985981f28ebbfc7314ddf33
Swathi Golakoti <59012114+swathi-code@users.noreply.github.com>
2022-07-08 15:08:38 +0530

    Updated

--- CoinDock-api/app/Http/Controllers/V1/CoincardController.php
+++ CoinDock-api/app/Http/Controllers/V1/CoincardController.php
@@ -6,24 +6,25 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\V1\User;
-use App\Models\V1\{Wallet,Coin};
+use App\Models\V1\{Wallet, Coin};
 use App\Http\Resources\V1\coinCardResource;
 
 
 class CoincardController extends Controller
 {
-     public function coinCard(User $user)
-     {
-        $coins=$user->wallets->map(function($wallet){return $wallet->coin;});
-        if($coins->isEmpty()){
-            return response(['message' => 'User have empty data'],400);
-        } else{
-             return response([
+    public function coinCard(User $user)
+    {
+        $coins = $user->wallets->map(function ($wallet) {
+            return $wallet->coin;
+        });
+        if ($coins->isEmpty()) {
+            return response(['message' => 'User have empty data'], 400);
+        } else {
+            return response([
                 'message' => 'success',
                 'results' => coinCardResource::collection($coins)->resolve(),
-                
-                ], 200);
-            }
-     }
-}
 
+            ], 200);
+        }
+    }
+}
