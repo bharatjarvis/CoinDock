a39e1fd456a6f638c957c04172ce4600b5d69c18
Swathi Golakoti <59012114+swathi-code@users.noreply.github.com>
2022-07-08 15:55:11 +0530

    Updated

--- CoinDock-api/app/Http/Resources/V1/coinCardResource.php
+++ CoinDock-api/app/Http/Resources/V1/coinCardResource.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Resources\V1;
+
 use App\Models\V1\Coin;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -16,17 +17,17 @@ class coinCardResource extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
-    
+
     public function toArray($request)
     {
         $coin = Coin::whereIsDefault(1)->first();
         return [
-                       'coin_name' => $this->coin_id,
-                        'logo' =>  $this->img_path,
-                         $coin->coin_id . '_coin'  =>  $this->defaultCoin(),
-                        'number_of_coins' =>  $this->countCoins(),
-                        'primary_currency' => $this->getPrimaryCurrency(),
-                        'secondary_currency' => $this->getSecondaryCurrency(),
+            'coin_name' => $this->coin_id,
+            'logo' =>  $this->img_path,
+            $coin->coin_id . '_coin'  =>  $this->defaultCoin(),
+            'number_of_coins' =>  $this->countCoins(),
+            'primary_currency' => $this->getPrimaryCurrency(),
+            'secondary_currency' => $this->getSecondaryCurrency(),
         ];
     }
 }
