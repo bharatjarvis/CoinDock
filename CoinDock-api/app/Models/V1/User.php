2982323b0caa2aac23efd7a81f61030733281feb
AKHIL-882 <51040667+AKHIL-882@users.noreply.github.com>
2022-07-18 14:38:18 +0530

    files modified

--- CoinDock-api/app/Models/V1/User.php
+++ CoinDock-api/app/Models/V1/User.php
@@ -5,6 +5,7 @@ namespace App\Models\V1;
 use App\Enums\V1\TimePeriod;
 use App\Enums\V1\UserStatus;
 use App\Enums\V1\UserType;
+use App\Exceptions\ApiKeyException;
 use App\Http\Requests\V1\ChartRequest;
 use App\Models\V1\{Signup,Setting};
 use App\Models\V1\{Coin};
@@ -142,6 +143,7 @@ class User extends Authenticatable
                 $response = Http::withHeaders([
                         'X-CoinAPI-Key'=>config('cryptohistoricaldata.coin.api_key')
                     ])->get($baseURLIdReplaced);
+
                 
                 $primaryBalance = Arr::get($response, 'rate', null)* $wallets[$key];
                 
@@ -172,7 +174,7 @@ class User extends Authenticatable
 
     public function getCoinId($coinId):array|collection
     {
-        if($coinId != 'All') {
+        if($coinId != 'Coins') {
             return $this->wallets->map(function($wallet) use($coinId) {
                     return $wallet->coin()->whereCoinId($coinId)->first();    
                 })->unique('coin_id')->pluck('coin_id')->filter();
