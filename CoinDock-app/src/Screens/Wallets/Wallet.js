2c8a46f820793878369062c71d42e641148bb61f
padala_sita <padalasita7@gmail.com>
2022-07-27 20:41:01 +0530

    Move to the previous state

--- CoinDock-app/src/Screens/Wallets/Wallet.js
+++ CoinDock-app/src/Screens/Wallets/Wallet.js
@@ -17,75 +17,75 @@ function Wallet() {
   }
 
   return (
-    <div className="cd-wallet-card cd-pagetitle mt-5000 container card p-3">
-      {/* <div className=""> */}
-      <div
-        className="cd-row-space-between"
-        style={{ display: "flex", width: "100%" }}
-      >
-        <h6>Wallets</h6>
-        <PlusCircle size={30} onClick={() => dispatch(openPopup())} />
-      </div>
+    <div className="cd-wallet-card cd-pagetitle mt-5000 container">
+      <div className="card p-3">
+        <div
+          className="cd-row-space-between"
+          style={{ display: "flex", width: "100%" }}
+        >
+          <h6>Wallets</h6>
+          <PlusCircle size={30} onClick={() => dispatch(openPopup())} />
+        </div>
 
-      {Object.values(coincard?.data?.results ?? {}).map((value) => {
-        return (
-          <div className="card-block" key={value.id}>
-            <div className="col-md-12 card p-3">
-              <div className="row">
-                <div className="col-md-2">
-                  <div className="photo-box">
-                    <h6>
-                      <img
-                        src={value?.logo}
-                        className="cd_coin_logo"
-                        alt={value.coin_name}
+        {Object.values(coincard?.data?.results ?? {}).map((value) => {
+          return (
+            <div className="card-block" key={value.id}>
+              <div className="col-md-12 card p-3">
+                <div className="row">
+                  <div className="col-md-2">
+                    <div className="photo-box">
+                      <h6>
+                        <img
+                          src={value?.logo}
+                          className="cd_coin_logo"
+                          alt={value.coin_name}
+                        />
+                      </h6>
+                    </div>
+                  </div>
+                  <div className="col-md-2">
+                    <div className="photo-box">
+                      <EllipseNumber
+                        component="h6"
+                        text={value?.BTC_coin?.toString() ?? ""}
+                        classNames="text-end"
+                        maxLetters={4}
                       />
-                    </h6>
-                  </div>
-                </div>
-                <div className="col-md-2">
-                  <div className="photo-box">
-                    <EllipseNumber
-                      component="h6"
-                      text={value?.BTC_coin?.toString() ?? ""}
-                      classNames="text-end"
-                      maxLetters={4}
-                    />
 
-                    <p className="mb-2 text-muted text-end">BTC</p>
+                      <p className="mb-2 text-muted text-end">BTC</p>
+                    </div>
                   </div>
-                </div>
-                <div className="col-md-2">
-                  <div className="photo-box">
-                    <h6 className="text-end">{value?.number_of_coins}</h6>
-                    <p className="mb-2 text-muted text-end">Coins</p>
+                  <div className="col-md-2">
+                    <div className="photo-box">
+                      <h6 className="text-end">{value?.number_of_coins}</h6>
+                      <p className="mb-2 text-muted text-end">Coins</p>
+                    </div>
                   </div>
-                </div>
-                <div className="col-md-2">
-                  <div className="photo-box">
-                    <h6 className="text-end">
-                      {value?.primary_currency.toFixed(2)}
-                    </h6>
-                    <p className="mb-2 text-muted text-end">
-                      {value?.primary_currency_code}
-                    </p>
+                  <div className="col-md-2">
+                    <div className="photo-box">
+                      <h6 className="text-end">
+                        {value?.primary_currency.toFixed(2)}
+                      </h6>
+                      <p className="mb-2 text-muted text-end">
+                        {value?.primary_currency_code}
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="col-md-2">
-                  <div className="photo-box">
-                    <h6 className="text-end">{value?.secondary_currency}</h6>
-                    <p className="mb-2 text-muted text-end">
-                      {value?.secondary_currency_code}
-                    </p>
+                  <div className="col-md-2">
+                    <div className="photo-box">
+                      <h6 className="text-end">{value?.secondary_currency}</h6>
+                      <p className="mb-2 text-muted text-end">
+                        {value?.secondary_currency_code}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
-    // </div>
   );
 }
 export default Wallet;
